df4b4721293d2f564705806f4c1c4fb738f3d63b
TeleDALAD <ashkankorouei@gmail.com>
2016-02-06 22:21:05 +0330

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -1,53 +1,29 @@
+
 do
-
- function run(msg, matches)
-
- 
-
-  local fuse = '📌 New #feedback\n\n👤 ID : ' .. msg.from.id .. '\n\n🔍 Name: ' .. msg.from.print_name ..'\n\n🈯️ Username: @' .. msg.from.username .. '\n\n 📝 The Pm:\n' .. matches[1] 
-
- local fuses = '!printf user#id' .. msg.from.id
-
- 
-
- 
-
-   local text = matches[1]
-
-   local chat = "chat#id"..90312082 
-
-   --like : local chat = "chat#id"..12345678
-
-   
-
-  local sends = send_msg(chat, fuse, ok_cb, false)
-
-  return 'Send msg for sudo tnx you'
-
- 
-
- end
-
- end
-
- return {
-
-  
-
-  description = "Feedback",
-
- 
-
-  usage = "!feedback message",
-
-  patterns = {
-
-  "^[!/][Ff]eedback (.*)$"
-
- 
-
-  },
-
-  run = run
-
- }
+ function run(msg, matches)
+ 
+ local fuse = 'New FeedBack\n\nId : ' .. msg.from.id .. '\n\nName: ' .. msg.from.print_name ..'\n\nUsername: @' .. msg.from.username .. '\n\nThe Pm:\n' .. matches[1] 
+ local fuses = '!printf user#id' .. msg.from.id
+ 
+ 
+   local text = matches[1]
+   local chat = "chat#id"..YourChatId 
+   --like : local chat = "chat#id"..12345678
+   
+  local sends = send_msg(chat, fuse, ok_cb, false)
+  return 'Sent!'
+ 
+ end
+ end
+ return {
+  
+  description = "Feedback",
+ 
+  usage = "!feedback message",
+  patterns = {
+  "^[!/][Ff]eedback (.*)$"
+ 
+  },
+  run = run
+ }
+     
