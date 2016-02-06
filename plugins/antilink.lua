80787841ce661cb5de499b21cb594a3fb7eddc99
TeleDALAD <ashkankorouei@gmail.com>
2016-02-06 15:00:15 +0330

    Update antilink.lua

--- plugins/antilink.lua
+++ plugins/antilink.lua
@@ -1,13 +1,30 @@
-local function run(msg)
-    if not is_momod(msg) and msg.type = 'chat' then
-        --chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-        return 'No links here!'
-    end
-end
 
+
+
+local function run(msg)
+    
+    local data = load_data(_config.moderation.data)
+    
+     if data[tostring(msg.to.id)]['settings']['antilink'] == 'yes' then
+      
+    
+if not is_momod(msg) then
+    
+    
+chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+    local msgads = 'ForbiddenAdText'
+   local receiver = msg.to.id
+    send_large_msg('chat#id'..receiver, msg.."\n", ok_cb, false)
+	
+      end
+   end
+end
+    
 return {patterns = {
-    ".com",
-    "http://",
-    "https://",
-    "adf.ly"
-    }, run = run}
+"[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+"[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]",
+"[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+"[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+"[Hh][Tt][Tt][Pp]://",
+"[Ww][Ww][Ww]:",
+}, run = run}
