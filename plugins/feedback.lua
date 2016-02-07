9f336847e9d645b55815c8e668c362e2cb0ebd40
TeleDALAD <ashkankorouei@gmail.com>
2016-02-07 15:11:02 +0330

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -1,29 +1,43 @@
-
 do
- function run(msg, matches)
- 
- local fuse = 'New FeedBack\n\nId : ' .. msg.from.id .. '\n\nName: ' .. msg.from.print_name ..'\n\nUsername: @' .. msg.from.username .. '\n\nThe Pm:\n' .. matches[1] 
- local fuses = '!printf user#id' .. msg.from.id
- 
- 
-   local text = matches[1]
-   local chat = "chat#id"..YourChatId 
-   --like : local chat = "chat#id"..12345678
-   
-  local sends = send_msg(chat, fuse, ok_cb, false)
-  return 'Sent!'
- 
- end
- end
- return {
-  
-  description = "Feedback",
- 
-  usage = "!feedback message",
+
+function run(msg, matches)
+
+local fuse = '#DearAdmin😜 we have recived a new feedback just now : #newfeedback \n\n🆔 : ' .. msg.from.id .. '\n\nNAME▶️ : ' .. msg.from.print_name ..'\n\nusername▶️ :@ ' .. msg.from.username  ..'\pm :\n\n' .. matches[1] 
+local fuses = '!printf user#id' .. msg.from.id
+
+
+    local text = matches[1]
+ bannedidone = string.find(msg.from.id, '123')
+        bannedidtwo =string.find(msg.from.id, '465')       
+   bannedidthree =string.find(msg.from.id, '678')  
+
+
+        print(msg.to.id)
+
+        if bannedidone or bannedidtwo or bannedidthree then                    --for banned people
+                return 'You are banned to send a feedback'
+ else
+
+
+                 local sends0 = send_msg('chat#59845789', fuse, ok_cb, false)
+
+ return 'sent! Tnx you'
+
+     
+
+end
+
+end
+return {
+  description = "Feedback to sudos",
+
+  usage = "!feedback : send maseage to admins with bot",
   patterns = {
-  "^[!/][Ff]eedback (.*)$"
- 
+    "^[!/]([Ff]eedback) (.*)$"
+
   },
   run = run
- }
-     
+}
+
+end
+
